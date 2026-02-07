bd545cf575371812b20f79a3bbcb91b9fb3c16ee
KeiranJamesCatalan <catalanka@students.nu-fairview.edu.ph>
2026-02-07 19:08:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,38 +15,36 @@
 </header>
 
 <table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Contents</th>
-            <th>Location</th>
-            <th>Status</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
+<thead>
+<tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Contents</th>
+    <th>Location</th>
+    <th>Status</th>
+    <th>Actions</th>
+</tr>
+</thead>
 
-    <tbody>
-    <?php
-    $sql = "SELECT * FROM kits";
-    $result = $conn->query($sql);
+<tbody>
+<?php
+$result = $conn->query("SELECT * FROM kits");
 
-    while($row = $result->fetch_assoc()) {
-        echo "<tr>
-            <td>{$row['id']}</td>
-            <td>{$row['kit_name']}</td>
-            <td>{$row['contents']}</td>
-            <td>{$row['location']}</td>
-            <td>{$row['status']}</td>
-            <td>
-                <a href='edit.php?id={$row['id']}'>Edit</a> |
-                <a href='delete.php?id={$row['id']}' onclick='return confirmDelete()'>Delete</a>
-            </td>
-        </tr>";
-    }
-    ?>
-    </tbody>
-
+while($row = $result->fetch_assoc()) {
+    echo "<tr>
+        <td>{$row['id']}</td>
+        <td>{$row['kit_name']}</td>
+        <td>{$row['contents']}</td>
+        <td>{$row['location']}</td>
+        <td>{$row['status']}</td>
+        <td>
+            <a href='edit.php?id={$row['id']}'>Edit</a> |
+            <a href='delete.php?id={$row['id']}' onclick='return confirmDelete()'>Delete</a>
+        </td>
+    </tr>";
+}
+?>
+</tbody>
 </table>
 
 </body>
