679e7b98dfecee2d007e814c0a309ae9c73f5e5b
KeiranJamesCatalan <catalanka@students.nu-fairview.edu.ph>
2026-02-07 19:05:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,35 +3,51 @@
 <html>
 <head>
     <title>Earthquake Emergency Kit Deployment</title>
-    <link rel="stylesheet" href="styles.css">
-    <script src="scripts.js" defer></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" defer></script>
 </head>
+
 <body>
-     <header>
-       <h1>Emergency Kits Dashboard</h1>
-       <a href="add.php">➕ Add New Kit</a>
-     </header>
-    <table>
+
+<header>
+    <h1>Emergency Kits Dashboard</h1>
+    <a href="add.php">➕ Add New Kit</a>
+</header>
+
+<table>
+    <thead>
         <tr>
-            <th>ID</th><th>Name</th><th>Contents</th><th>Location</th><th>Status</th><th>Actions</th>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Contents</th>
+            <th>Location</th>
+            <th>Status</th>
+            <th>Actions</th>
         </tr>
-        <?php
-        $sql = "SELECT * FROM kits";
-        $result = $conn->query($sql);
-        while($row = $result->fetch_assoc()) {
-            echo "<tr>
-                <td>".$row['id']."</td>
-                <td>".$row['kit_name']."</td>
-                <td>".$row['contents']."</td>
-                <td>".$row['location']."</td>
-                <td>".$row['status']."</td>
-                <td>
-                    <a href='edit.php?id=".$row['id']."'>Edit</a> |
-                    <a href='delete.php?id=".$row['id']."' onclick='return confirmDelete();'>Delete</a>
-                </td>
-            </tr>";
-        }
-        ?>
-    </table>
+    </thead>
+
+    <tbody>
+    <?php
+    $sql = "SELECT * FROM kits";
+    $result = $conn->query($sql);
+
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>
+            <td>{$row['id']}</td>
+            <td>{$row['kit_name']}</td>
+            <td>{$row['contents']}</td>
+            <td>{$row['location']}</td>
+            <td>{$row['status']}</td>
+            <td>
+                <a href='edit.php?id={$row['id']}'>Edit</a> |
+                <a href='delete.php?id={$row['id']}' onclick='return confirmDelete()'>Delete</a>
+            </td>
+        </tr>";
+    }
+    ?>
+    </tbody>
+
+</table>
+
 </body>
 </html>
